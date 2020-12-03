5308521a133bb265ffe299e88bf21af8ce90e552
StefanTuczynski <75397517+StefanTuczynski@users.noreply.github.com>
2020-12-03 00:41:41 -0500

    Update avg_marks.py

--- avg_marks.py
+++ avg_marks.py
@@ -1,5 +1,15 @@
-Course_One = int(input("Course One: "))
-Course_Two = int(input("Course Two: "))
-Course_Three = int(input("Course Three: "))
-Course_Four = int(input("Course Four :"))
-print("Average: " + str((Course_One + Course_Two + Course_Three + Course_Four) / 4) + "%")
+"""
+Name: avg_marks.py
+Purpose: To calculate the total average between 4 course averages
+Creator: Stefan Tuczynski
+Date: 02/12/2020
+"""
+
+#These lines takes the users 4 course's average
+Course_One = int(input("Course One Average: "))
+Course_Two = int(input("Course Two Average: "))
+Course_Three = int(input("Course Three Average: "))
+Course_Four = int(input("Course Four Average: "))
+
+#This line calculates the total average between the 4 courses
+print("Your total average is: " + str((Course_One + Course_Two + Course_Three + Course_Four) / 4) + "%")
